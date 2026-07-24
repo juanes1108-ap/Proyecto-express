5d613c08462e2091c5c46657d4eab4ec5942cc3c
Juan Esteban Angulo Perez <juanestebananguloperez75@gmail.com>
2026-07-24 11:40:25 -0500

    configurar la variable de entorno

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (_, res) => {
-  res.send('Aprendiendo express, ficha 3407181');
+  res.send(`Aprendiendo express, ficha 3407181, ADSO en el curso de desarrollo web`);
 });
 
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto: ${port}`);
-});
+}); 
