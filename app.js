0bba6f0776c63745345b65349507d3d4a098ec1c
esteban <juanesap>
2026-07-31 10:25:49 -0500

    configuracion de entorno virtual parte 3

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+//const express = require("express")
 import express from 'express';
+
+//leer el archivo .env
+import {configDotenv} from "dotenv"
+configDotenv()
+
 const app = express();
-const port = process.env.PUERTO || 3000;
+const port = process.env.PUERTO || 5050;
 
 app.get("/", (_, res) => {
-  res.send(`Aprendiendo express, ficha 3407181, ADSO en el curso de desarrollo web`);
+  res.send(`Aprendiendo express, ficha 3407181, ADSO en el curso de desarrollo web el 31 de julio de 2026`);
 });
+
+//otro endpoint
+
 
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto: ${port}`);
